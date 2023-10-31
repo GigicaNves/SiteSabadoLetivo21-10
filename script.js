9e2d3c193c5ea1f18e692461b3148b38993d34cd
Giovanna Neves Rodrigues <giovanna.neves2007@gmail.com>
2023-10-31 17:21:20 -0300

    V1.3.0
    Versão final

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-var SabadoLetivo = document.querySelector("h3.sabado");
+const btnScrollToTop = document.getElementById("btnscrollToTop");
+
+btnScrollToTop.addEventListener("click", function () {
+
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    })
+
+
+})
